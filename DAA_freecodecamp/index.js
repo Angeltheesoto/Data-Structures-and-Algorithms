5d367614f1e91112b3a8a6a68bb999d77402fd6f
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-07-20 11:51:40 -0400

    Working with freecodecamp

---
+++ DAA_freecodecamp/index.js
@@ -0,0 +1,16 @@
+//  Understanding Arrays
+
+/*
+ Lookup O(1)
+ Insert O(n)
+ Delete O(n)
+*/
+
+// Demo
+function main(args) {
+ const arr = new Array(3);
+ arr.push(10)
+ arr.push(20)
+ arr.push(30)
+}
+main(arr)
